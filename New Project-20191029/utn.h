5ba2072adce127956a94137d415a3738f3b943d5
LudmilaCisneros <47437126+LudmilaCisneros@users.noreply.github.com>
2019-10-29 11:31:51 -0300

    Created by Coding Ground Client

---
+++ New Project-20191029/utn.h
@@ -0,0 +1,22 @@
+#ifndef UTN_H_INCLUDED
+#define UTN_H_INCLUDED
+
+//VALIDAR//
+int utn_isValidInt(char *string);
+int utn_isValidFloat(char *string);
+int utn_isValidName(char *string);
+int utn_isValidAddress(char *string);
+
+//OBTENER//
+int utn_getString(char *pResultado, char *mensaje, char *mensajeError, int min,
+		int max, int intentos);
+int utn_getInt(int *pResultado, char *mensaje, char *mensajeError, int min,
+		int max, int intentos);
+int utn_getFloat(float *pResultado, char *mensaje, char *mensajeError, int min,
+		int max, int intentos);
+int utn_getName(char *pResultado, int len, char *mensaje, char *mensajeError,
+		int minChars, int maxChars, int intentos);
+int utn_getAddress(char *pResultado, int len, char *mensaje, char *mensajeError,
+		int minChars, int maxChars, int intentos);
+
+#endif // UTN_H_INCLUDED
