f61336f8a101048ac0aa4141ebdfbbc89178d4bb
LudmilaCisneros <47437126+LudmilaCisneros@users.noreply.github.com>
2019-10-29 11:23:17 -0300

    Created by Coding Ground Client

---
+++ New Project-20191029/menu.h
@@ -0,0 +1,10 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+///////////////MENU.H///////////////
+
+void menu(sCliente* listClient,sPedidos* listPedidos, int lenClient, int lenPedidos);
+
+
+
+#endif /* MENU_H_ */
